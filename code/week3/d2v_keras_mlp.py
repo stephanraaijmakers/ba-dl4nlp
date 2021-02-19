fc91d01f4b348a76b5ec106139f013cd2687ec20
Stephan Raaijmakers <stephan.raaijmakers@tno.nl>
2021-02-19 11:49:06 +0100

    Update

--- code/week3/d2v_keras_mlp.py
+++ code/week3/d2v_keras_mlp.py
@@ -73,6 +73,7 @@ class MyCorpus:
 def build_d2v_model(dimension):
     texts= MyCorpus()
     documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(texts)]
+    print("Building doc2vec model....")
     model = Doc2Vec(documents, vector_size=dimension, window=2, min_count=1, workers=4)
     return (documents, model)
     
