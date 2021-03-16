133ec30cc750411c31ff85e72ee8067ab20a94f8
Stephan Raaijmakers <stephan.raaijmakers@tno.nl>
2021-03-16 23:51:11 +0100

    Update

--- code/week7/hierarchical-attention-sentiment.py
+++ code/week7/hierarchical-attention-sentiment.py
@@ -34,7 +34,7 @@ review_texts = open(filename, 'r', encoding='ISO-8859-1').readlines()
 Words={}
 Labels={}
 
-max_sent_len=30
+max_sent_len=100
 max_review_len=5
 
 labels=[]
@@ -59,7 +59,7 @@ vocab_len=len(Words)+1
 word_to_int = dict((w, i+1) for i, w in enumerate(words))
 word_to_int[0]=0 # padding
 int_to_word = dict((i+1, w) for i, w in enumerate(words))
-int_to_word[0]=0 # padding
+int_to_word[0]="-PAD-" # padding
 
 label_names=sorted(Labels)
 label_to_int = dict((w, i) for i, w in enumerate(label_names))
@@ -71,7 +71,7 @@ for i in range(len(labels)):
 
 max_review_len=15
 max_sent_len = 100
-max_words = 20000
+max_words = 50000
 
 
 data = np.zeros((len(reviews), max_review_len, max_sent_len), dtype='int32')
@@ -93,11 +93,11 @@ data = data[indices]
 labels = labels[indices]
 y=labels
 
+#data=data[:1000]
+#y=y[:1000]
+
 validation_split=0.1
 nb_validation_samples = int(validation_split * data.shape[0])
-
-data=data[:1000]
-y=y[:1000]
 
 
 X_train = data[:-nb_validation_samples]
@@ -132,53 +132,57 @@ model.summary()
 
 #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
-model.fit(X_train,y_train,epochs=1,batch_size=64)
+model.fit(X_train,y_train,epochs=10,batch_size=64)
 
 
 #predictions=model.predict(X_test)
 
-n=0
-for x_test in X_test:
-        #word_attention_map = get_activations(sentence_encoder, np.array([x_test]), layer_names='attention_weight')
-        sentence_attention_map = get_activations(model, np.array([x_test]), layer_names='attention_weight')
-        for sent in x_test:
-                word_attention_map = get_activations(sentence_encoder, np.array([sent]), layer_names='attention_weight')
-        # Next combine weights: per sentence, per word
-        print("YES")
-        exit(0)
-        #print(attention_map['attention_weight'][0])
-        a=attention_map['attention_weight'][0]
-        print(a)
-        exit(0)
-        print_a=False
-        for i in range(len(a)):
-                if i!=focus_position-1:
-                        if a[i]>0.05:
-                                print_a=True
-                
-        if print_a:
-                print("Found:",n)
-                xvals=np.arange(len(x_test))
-                words=tuple([int_to_word[w] for w in x_test])
-                plt.xticks(xvals,words)
-                plt.imshow(attention_map['attention_weight'], cmap=cm.afmhot) # cmap='hot')
-                plt.title(f'Attention')
-                plt.savefig(f'attention-%d.png'%(n))
-                n+=1
-        #plt.close()
+def normalize(probs):
+        prob_factor = 1.0 / sum(probs)
+        return [prob_factor * p for p in probs]
 
-        
+def to_words(x):
+        s=""
+        for w in x:
+                if w!=0: # padding
+                        s+=int_to_word[w]+" "
+        return(s.rstrip())
+
 n=0
-for sentence in X_test:
-        sentence_prediction=predictions[n]
-        n+=1
-        best_tag=int_to_tag[np.argmax(sentence_prediction,axis=0)]
-        m=1
-        for int_word in sentence:
-            if m==focus_position:
-                print("<<%s_%s>>"%(int_to_word[int_word],best_tag),end=" ")
-            else:
-                print(int_to_word[int_word],end=" ")
-            m+=1
+for x_test in X_test[:10]:
+        prediction=model.predict(np.array([x_test]))[0]
+        sentence_attention = get_activations(model, np.array([x_test]), layer_names='attention_weight')
+        sent_probs=sentence_attention['attention_weight'][0]
+        print(sent_probs)
+        for x in x_test:
+                print(to_words(x),end=" ")
         print()
-              
+        m=np.argmax(sent_probs,axis=0)
+        most_important_sentence=x_test[m]
+        print("MOST IMPORTANT SENT=% (%f)"%(to_words(most_important_sentence),sent_probs[m]))
+        word_attention = get_activations(sentence_encoder, np.array([most_important_sentence]), layer_names='attention_weight')
+        word_probs=word_attention['attention_weight'][0]
+        word_probs=normalize(word_probs)
+        print("NORM wprobs:",word_probs)
+        i=0
+        for word in word_probs:
+                if most_important_sentence[i]!=0: #padding
+                        print("%s:%f"%(int_to_word[most_important_sentence[i]],word_probs[i]),end="|")
+                i+=1
+        pred_label=int_to_label[np.argmax(prediction,axis=0)]
+        gt_label=int_to_label[np.argmax(y_test[n],axis=0)]      
+        print("\nLabel=%s Prediction=%s"%(gt_label,pred_label))
+        
+        #for sent in x_test:
+                #word_attention_map = get_activations(sentence_encoder, np.array([sent]), layer_names='attention_weight')
+        # Next combine weights: per sentence, per word
+
+        xvals=np.arange(len(x_test))
+        words=tuple([int_to_word[w] for w in most_important_sentence])
+        plt.xticks(xvals,words)
+        wp=np.array([word_probs])
+        plt.imshow(wp, cmap=cm.afmhot) # cmap='hot')
+        plt.title(f'Attention gt=%s pred=%s'%(gt_label,pred_label))
+        plt.savefig(f'hierarchical-attention-%d.png'%(n))
+        
+        n+=1
