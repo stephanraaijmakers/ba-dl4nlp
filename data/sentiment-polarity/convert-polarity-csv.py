0285eff3ca688b398b442b849fc2e0df827ded86
Stephan Raaijmakers <stephan.raaijmakers@tno.nl>
2021-05-07 15:03:56 +0200

    Update

---
+++ data/sentiment-polarity/convert-polarity-csv.py
@@ -0,0 +1,19 @@
+import sys
+import re
+
+
+if __name__=="__main__":
+    f=open(sys.argv[1],"r")
+    print("review,label")
+    for line in f:
+        line=line.rstrip()
+        m=re.match("^(.+)[\s\t]+\.[\s\t]+([^\s]+).*$",line)
+        if m:
+            label=m.group(2)
+            if label=="0":
+                label="neg"
+            else:
+                label="pos"
+            print("\"%s\",%s"%(m.group(1),label))
+    f.close()
+    
